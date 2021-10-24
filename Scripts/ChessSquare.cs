ef16cbc7719be171a957d2f85c0fdc61a77e0583
joseChQ <63762044+joseChQ@users.noreply.github.com>
2021-10-24 16:04:57 -0500

    Add files via upload

---
+++ Scripts/ChessSquare.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChessSquare : MonoBehaviour {
+
+	// Use this for initialization
+
+	// this it the scrpt that manages the piece movement
+	PieceMovement piecemovScript;
+
+	// this is the over material used when focusing on the square
+	public Material overMat;
+    public string name_square;
+    // pointer functions is used to call the pepare to click
+    public bool canMove;
+    public Piece pieceToCapture;
+
+	void Start ()
+	{
+		piecemovScript=GameObject.FindGameObjectWithTag("pieceMovement").GetComponent<PieceMovement>();
+        gameObject.GetComponent<Renderer>().material = overMat;
+        gameObject.GetComponent<Renderer>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	//*****************
+	// call to the movement function in this case
+	//*******************
+	public  bool onClickSquare ()
+	{
+        if (canMove)
+        {
+            
+            if(pieceToCapture!=null)
+            {
+                piecemovScript.movePiece(gameObject, pieceToCapture);
+                return true;
+            }
+            else
+            {
+                piecemovScript.movePiece(gameObject, null);
+            }
+            
+        }
+        return false;
+
+	}
+
+    public void active(Piece pc)
+    {
+        gameObject.GetComponent<Renderer>().enabled =true;
+        canMove = true;
+        pieceToCapture = pc;
+
+    }
+
+	public void onExitSquare ()
+	{
+        gameObject.GetComponent<Renderer>().enabled = false;
+        canMove = false;
+        pieceToCapture = null;
+        
+    }
+}
---
+++ Scripts/ControlBoton.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TextSpeech;
+using UnityEngine.UI;
+using UnityEngine.Android;
+public class ControlBoton : MonoBehaviour
+{
+	const string LANG_CODE = "es-ES";
+
+	[SerializeField]
+	Text uiText;
+	[SerializeField]
+	Text uiText1;
+
+	PieceMovement piecemovScript;
+
+	public int turno;
+	public float th = 0.008f;
+
+	void Start()
+	{
+		Setup(LANG_CODE);
+
+		SpeechToText.instance.onPartialResultsCallback = OnPartialSpeechResult;
+
+
+		SpeechToText.instance.onResultCallback = OnFinalSpeechResult;
+		piecemovScript = GameObject.FindGameObjectWithTag("pieceMovement").GetComponent<PieceMovement>();
+		CheckPermission();
+	}
+	void CheckPermission()
+	{
+
+		if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+		{
+			Permission.RequestUserPermission(Permission.Microphone);
+		}
+
+	}
+
+	#region Speech to text
+	public void StartListening()
+	{
+		SpeechToText.instance.StartRecording();
+	}
+	public void StopListening()
+	{
+		SpeechToText.instance.StopRecording();
+	}
+	void OnFinalSpeechResult(string result)
+	{
+		instance(result);
+		uiText1.text = result;
+
+	}
+	void OnPartialSpeechResult(string result)
+	{
+		instance(result);
+		uiText1.text = result;
+	}
+	#endregion
+	void Setup(string code)
+	{
+	//	TextToSpeech.instance.Setting(code, 1, 1);
+		SpeechToText.instance.Setting(code);
+	}
+	public  void IsOccupied(GameObject sq)
+	{
+		//bool occupied = false;
+
+		turno = piecemovScript.playerTurn;
+		foreach (Piece pc in piecemovScript.allPieces)
+		{
+			int color = pc.GetComponent<Piece>().color;
+			if ((pc.transform.position - sq.transform.position).magnitude < th && color == turno )
+            {
+				pc.GetComponent<Piece>().onPieceClick(color);
+			}	
+		}
+
+		//return occupied;
+	}
+	void instance(string name_square)
+    {
+		string name_square1 = name_square.ToLower();
+		GameObject choosed_square= null;
+		string tmp = "Comando no permitido";
+
+		
+		foreach (GameObject sq in piecemovScript.squares)
+        {
+			string my_name = sq.GetComponent<ChessSquare>().name_square;
+			if (my_name.Equals(name_square1))
+            {
+				tmp = "...";
+				choosed_square = sq;
+			}
+        }
+		uiText.text = tmp;
+		if (choosed_square != null)
+        {
+			if(choosed_square.GetComponent<ChessSquare>().onClickSquare() == false)
+            {
+				IsOccupied(choosed_square);
+			}	
+		}
+
+	}
+}
